9a436cbb32aedf089c68e6a050b06ba2188244aa
Callan Patay <callan2.patay@live.uwe.ac.uk>
2019-04-14 16:26:07 +0100

    given the applicaiton a bar

--- DirectXAT/DirectXAT/SystemClass.cpp
+++ DirectXAT/DirectXAT/SystemClass.cpp
@@ -217,7 +217,7 @@ void SystemClass::InitializeWindows(int& screenWidth, int& screenHeight)
 	m_hinstance = GetModuleHandle(NULL);
 
 	// Give the application a name.
-	m_applicationName = L"Engine";
+	m_applicationName = L"Procedural Building Generation";
 
 	// Setup the windows class with default settings.
 	wc.style = CS_OWNDC;
@@ -270,7 +270,7 @@ void SystemClass::InitializeWindows(int& screenWidth, int& screenHeight)
 
 	// Create the window with the screen settings and get the handle to it.
 	m_hwnd = CreateWindowEx(WS_EX_CLIENTEDGE, m_applicationName, m_applicationName,
-		WS_CLIPSIBLINGS | WS_CLIPCHILDREN | WS_POPUP,
+		WS_OVERLAPPED,
 		posX, posY, screenWidth, screenHeight, NULL, NULL, m_hinstance, NULL);
 
 	// Bring the window up on the screen and set it as main focus.
